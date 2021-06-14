855c0951317ee225f6717b689403728406ec62f7
Blaž Hrastnik <blaz@mxxn.io>
2021-06-15 01:33:34 +0900

    Update to 0.2.1

--- Formula/helix.rb
+++ Formula/helix.rb
@@ -1,8 +1,8 @@
 class Helix < Formula
   desc "Post-modern modal text editor"
   homepage "https://helix-editor.com"
-  url "https://github.com/helix-editor/helix/releases/download/v0.2.0/helix-v0.2.0-x86_64-macos.tar.xz"
-  sha256 "9cef20be98d033d45785da9df1db72ce8957ba6afe38eb1f4976c18753fbb31e"
+  url "https://github.com/helix-editor/helix/releases/download/v0.2.1/helix-v0.2.1-x86_64-macos.tar.xz"
+  sha256 "ba095274ea9d2c6202f7252344fdd91fb67b95f24aa0099cfd956e3b2c8e5735"
   license "MPL-2.0"
 
   bottle :unneeded
