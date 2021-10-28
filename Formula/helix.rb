7baef9eb259fce9e912554c01409a7ed771b0dd5
Blaž Hrastnik <blaz@mxxn.io>
2021-10-28 16:59:17 +0900

    Update to 0.5.0

--- Formula/helix.rb
+++ Formula/helix.rb
@@ -1,8 +1,8 @@
 class Helix < Formula
   desc "Post-modern modal text editor"
   homepage "https://helix-editor.com"
-  url "https://github.com/helix-editor/helix/releases/download/v0.4.1/helix-v0.4.1-x86_64-macos.tar.xz"
-  sha256 "4789acfa22c996ece3d4069422c86177b062f099d7fccaf8a253c9ad9553759a"
+  url "https://github.com/helix-editor/helix/releases/download/v0.5.0/helix-v0.5.0-x86_64-macos.tar.xz"
+  sha256 "6f71c420a7ff19db32717765129fc02f327974f718d6905307d02498a5bc1b08"
   license "MPL-2.0"
 
   def install
